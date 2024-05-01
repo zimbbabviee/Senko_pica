a74e9b1f1680c72c663597a1161159223891f719
Jevgenija <jevgenijasenjko@sk.lvt.lv>
2024-05-01 18:22:17 +0300

    Izstrāda menu

--- src/lvt/pica.java
+++ src/lvt/pica.java
@@ -1,9 +1,38 @@
 package lvt;
 
+import java.util.Arrays;
+
+import javax.swing.*;
 public class pica {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String izvele;
+		int izveleIndekss;
+		String[] darbibas = {"Reģistrēt klientu", "Jauns pasūtījums", "Aktīvie pasūtījumi", "Pabeigtie pasūtiījumi",
+				"Pabeigt pasūtījumu", "Beigt darbu"};
+		
+		
+		do {
+			izvele = (String)JOptionPane.showInputDialog(null, "Izvēlies darbību: ", "Izvele",
+					JOptionPane.QUESTION_MESSAGE,null, darbibas, darbibas[0]);
+			izveleIndekss = Arrays.asList(darbibas).indexOf(izvele);
+			switch(izveleIndekss) {
+			case 0:
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				JOptionPane.showMessageDialog(null,"Uz redzēšanos!", "Programmas apturēšana", 
+						JOptionPane.PLAIN_MESSAGE);
+				break;
+			}
+		}while(izveleIndekss !=5);
 
 	}
 
--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,5 @@
  *
  */
 module Pica {
+	requires java.desktop;
 }
